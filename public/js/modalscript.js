191c8ed75c9b4b34df9b89108b2e822920ccbedb
IndiaSuthar <bharat.bhushan@mtxb2b.com>
2017-10-30 11:40:58 +0530

    Update modalscript.js

--- public/js/modalscript.js
+++ public/js/modalscript.js
@@ -1,27 +1,6 @@
 'use strict';
 // Get the modal
-			function loadScript(url, callback)
-			{
-			// Adding the script tag to the head as suggested before
-			var head = document.getElementsByTagName('head')[0];
-			var script = document.createElement('script');
-			script.type = 'text/javascript';
-			script.src = url;
-
-			// Then bind the event to the callback function.
-			// There are several events for cross browser compatibility.
-			script.onreadystatechange = callback;
-			script.onload = callback;
-
-			// Fire the loading
-			head.appendChild(script);
-			}
-			function loadscripts(){
-				loadScript('https://cztestapp.herokuapp.com/js/controllers/main.js');
-				loadScript('https://cztestapp.herokuapp.com/js/services/todos.js');
-				loadScript('https://cztestapp.herokuapp.com/js/core.js');
-			}
-			loadScript('https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min.js',loadscripts);
+			
 			
 			
 			var modal = null;
